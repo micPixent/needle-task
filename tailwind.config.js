ac87d8221bb7b80dc4fde0ed2076850f483e6b59
micPixent <162548204+micPixent@users.noreply.github.com>
2024-08-14 01:53:06 +0800

    chore: font family FuturaPT init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -5,7 +7,49 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["FuturaPT", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: {
+                    50: "#fefa3",
+                    100: "#fdf0db",
+                    200: "#fbe7c3",
+                    300: "#f9ddab",
+                    400: "#f7ce87",
+                    500: "#f6c878",
+                    600: "#f3b64c",
+                    700: "#f1ac34",
+                    800: "#e3960f",
+                    900: "#9b660a",
+                },
+                secondary: {
+                    50: "#fcfdf9",
+                    100: "#f7f9f1",
+                    200: "#f1f5e9",
+                    300: "#ecf2e0",
+                    400: "#e6edd5",
+                    500: "#dde7c7",
+                    600: "#c7d0b3",
+                    700: "#a6ad95",
+                    800: "#858b77",
+                    900: "#63685a",
+                },
+                tertiary: {
+                    50: "#fbfcfc",
+                    100: "#f6f8f9",
+                    200: "#f0f4f5",
+                    300: "#ebf0f1",
+                    400: "#e3eaec",
+                    500: "#dae3e5",
+                    600: "#c4ccce",
+                    700: "#a4aaac",
+                    800: "#838889",
+                    900: "#626667",
+                },
+            },
+        },
     },
     plugins: [],
 }
