6928cbb0581640fe1bc6ef748a5503765b2d29a9
micPixent <162548204+micPixent@users.noreply.github.com>
2024-08-14 02:46:23 +0800

    add: form text input field & default config

---
+++ src/components/Forms/TextInput.tsx
@@ -0,0 +1,111 @@
+import { InputHTMLAttributes, ReactNode } from 'react'
+import TextInputConfig from './textInput.config'
+import { classNames } from '../../libs/classNameUtils'
+
+type Props = InputHTMLAttributes<HTMLInputElement> & {
+  label?: string
+  mode?: 'default' | 'border-bottom'
+  withValidation?: boolean
+  subtitle?: string | ReactNode
+  subtitleClassName?: string
+  error?: boolean
+}
+export default function TextInput({
+  name,
+  label,
+  placeholder,
+  type = 'text',
+  mode = 'default',
+  withValidation,
+  id,
+  className,
+  required,
+  disabled,
+  subtitle,
+  autoCapitalize,
+  subtitleClassName,
+  error,
+  ...rest
+}: Props) {
+  if (mode === 'border-bottom') {
+    return (
+      <div>
+        {label && (
+          <label
+            htmlFor={id}
+            className={classNames(
+              disabled ? '!text-grey-100' : '',
+              'block text-sm font-medium leading-6 text-grey-800',
+            )}
+          >
+            {label}
+            {required && <span className="ml-1 text-red-500">*</span>}
+          </label>
+        )}
+        {subtitle && (
+          <span className={classNames('text-xs text-black', subtitleClassName)}>
+            {subtitle}
+          </span>
+        )}
+        <div className="relative flex">
+          <input
+            id={id}
+            type={type}
+            name={name}
+            className={classNames(
+              TextInputConfig.TextInputMode[mode],
+              className,
+              disabled ? 'text-grey-100' : '',
+            )}
+            placeholder={placeholder}
+            disabled={disabled}
+            autoCapitalize={autoCapitalize}
+            {...rest}
+          />
+          {withValidation && (
+            <img src="/assets/icons/green-tick-icon.svg" alt="" />
+          )}
+          <div
+            className={classNames(
+              'absolute inset-x-0 bottom-0 border-t peer-focus:border-t-2',
+              error
+                ? 'border-red-500'
+                : 'border-grey-200 peer-focus:border-indigo-600',
+            )}
+            aria-hidden="true"
+          />
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {label && (
+        <label
+          htmlFor={id}
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          {label}
+        </label>
+      )}
+      <div className="mt-2 flex">
+        <input
+          type={type}
+          name={name}
+          id={id}
+          className={classNames(TextInputConfig.TextInputMode[mode], className)}
+          placeholder={placeholder}
+          disabled={disabled}
+          autoCapitalize={autoCapitalize}
+          {...rest}
+        />
+        {withValidation && (
+          <img src="/assets/icons/green-tick-icon.svg" alt="" />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export type { Props as TextInputProps }
---
+++ src/components/Forms/textInput.config.ts
@@ -0,0 +1,12 @@
+const TextInputMode = {
+	default:
+		"block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6",
+	"border-bottom":
+		"peer block w-full border-0 bg-gray-50 py-1.5 text-gray-900 focus:ring-0 focus:outline-none text-base leading-6 bg-white pl-0",
+};
+
+const TextInputConfig = {
+	TextInputMode,
+};
+
+export default TextInputConfig;
