8e1366e08d298d3d5f8bb4034ebff02b57a7ec72
micPixent <162548204+micPixent@users.noreply.github.com>
2024-08-14 22:22:30 +0800

    feat: base layout setup for login page

---
+++ src/layout/BaseLayout.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Container from '../components/Container/Container'
+import Button from '../components/Buttons'
+import Text from '../components/Typography/Text'
+import { classNames } from '../libs/classNameUtils'
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
+
+interface BaseLayoutProps {
+  className?: string
+  enableBackButton?: boolean
+  children: ReactNode
+}
+
+const BaseLayout = ({
+  children,
+  className,
+  enableBackButton = true,
+}: BaseLayoutProps) => {
+  const navigate = useNavigate()
+  return (
+    <div className={classNames('w-full mt-4 mx-8', className)}>
+      <Container>
+        {enableBackButton && (
+          <Container className="mr-2">
+            <Button mode="text" className="" onClick={() => navigate(-1)}>
+              <Container className="flex flex-row items-center text-primary-500 space-x-2">
+                <ChevronLeftIcon className="w-5 h-5" />
+                <Text className="text-primary-300">Back</Text>
+              </Container>
+            </Button>
+          </Container>
+        )}
+        {children}
+      </Container>
+    </div>
+  )
+}
+
+export default BaseLayout
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,12 @@
+import Container from '../components/Container/Container'
+import BaseLayout from '../layout/BaseLayout'
+
 const Login = () => {
-  return <></>
+  return (
+    <BaseLayout>
+      <Container>Login now to save your favourite dogs</Container>
+    </BaseLayout>
+  )
 }
 
 export default Login
