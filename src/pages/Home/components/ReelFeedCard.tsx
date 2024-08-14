25c166a6cd69f5a7e9781ffc4f143d160402699a
micPixent <162548204+micPixent@users.noreply.github.com>
2024-08-15 01:31:05 +0800

    feat: selection list of dog breeds

--- src/pages/Home/components/ReelFeedCard.tsx
+++ src/pages/Home/components/ReelFeedCard.tsx
@@ -3,19 +3,49 @@ import Container from '../../../components/Container/Container'
 import Title from '../../../components/Typography/Title'
 import Text from '../../../components/Typography/Text'
 import Button from '../../../components/Buttons'
+// import Image from '../../../components/Image/Image'
+import { useEffect, useState } from 'react'
+import { PlusCircleIcon } from '@heroicons/react/24/solid'
+import { classNames } from '../../../libs/classNameUtils'
 
 const ReelFeedCard = () => {
+  const [breeds, setBreeds] = useState([])
+
+  useEffect(() => {
+    const fetchBreeds = async () => {
+      try {
+        const response = await fetch('https://dog.ceo/api/breeds/list/all')
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        const data = await response.json()
+        setBreeds(data.message)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchBreeds()
+  }, [])
+
   return (
     <Card className="p-10 mt-10 basis-3/4">
       <Container className="flex justify-between mb-4">
-        <Title className="text-2xl">Welcome to Dog Show Reels</Title>
-        <Button className="w-32">View Feed</Button>
+        <Title className="text-2xl">Discover All Amazing Dog Breeds Here</Title>
+        <Button className="w-fit">View More Feed</Button>
       </Container>
-      Here have Image
-      <Container className="flex justify-between">
-        <Text className="mt-5 text-xl">
-          Enjoy our amazing dog photos that will make your day smile!
-        </Text>
+
+      <Container className="flex flex-wrap space-x-4 w-full mt-10 items-center">
+        {Object?.keys(breeds)
+          ?.slice(0, 7)
+          ?.map((breed, index) => (
+            <Container key={index} className="mb-5">
+              <Button className={classNames('w-full flex')} mode="outline">
+                <Text>{breed}</Text>
+                <PlusCircleIcon className="w-5 h-5 my-auto ml-3" />
+              </Button>
+            </Container>
+          ))}
       </Container>
     </Card>
   )
