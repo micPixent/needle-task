85b5ef3e123f17cbdb96813bf7d45f1027f4fa31
micPixent <162548204+micPixent@users.noreply.github.com>
2024-08-14 21:48:13 +0800

    chore: base homepage layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,51 @@
+import Button from '../components/Buttons'
 import Card from '../components/Card/Card'
 import Container from '../components/Container/Container'
 import Text from '../components/Typography/Text'
 import Title from '../components/Typography/Title'
+import useAuthContext from '../modules/Auth/useAuthContext'
 
 const Home = () => {
+  const { user } = useAuthContext()
+  console.log(user)
+
   return (
     <Container>
       <Container className="p-5">
         <Card className="p-10">
-          <Title className="text-2xl">Welcome to Dog Show Reels</Title>
-          <Text className="mt-5 text-xl">
-            Enjoy our amazing dog photos that will make your day smile!
-          </Text>
+          <Container className="flex justify-between mb-4">
+            <Title className="text-2xl">Welcome to Dog Show Reels</Title>
+            <Button className="w-32">Login Now</Button>
+          </Container>
+          <Container className="flex justify-between">
+            <Text className="mt-5 text-xl">
+              Enjoy our amazing dog photos that will make your day smile!
+            </Text>
+            <Button className="w-32">Register</Button>
+          </Container>
         </Card>
+
+        <Container className="flex space-x-10">
+          <Card className="p-10 mt-10 basis-3/4">
+            <Container className="flex justify-between mb-4">
+              <Title className="text-2xl">Welcome to Dog Show Reels</Title>
+              <Button className="w-32">Login Now</Button>
+            </Container>
+            Here have Image
+            <Container className="flex justify-between">
+              <Text className="mt-5 text-xl">
+                Enjoy our amazing dog photos that will make your day smile!
+              </Text>
+              <Button className="w-32">Register</Button>
+            </Container>
+          </Card>
+
+          <Card className="p-10 mt-10 basis-1/4">
+            <Container className="flex justify-between mb-4">
+              <Title className="text-2xl">My Saved Doggos</Title>
+            </Container>
+          </Card>
+        </Container>
       </Container>
     </Container>
   )
